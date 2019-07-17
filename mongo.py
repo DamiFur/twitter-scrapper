49737bd240eb297c4f5fc0c70c0355e08b214af0
damifur <damian.a.furman@gmail.com>
2019-07-17 14:31:07 -0300

    little fix

--- mongo.py
+++ mongo.py
@@ -10,7 +10,7 @@ def setup(collection):
         default_language='spanish')
 
 def setup_users():
-    db.users.create_index("id", unique=True)
+    db.users.create_index("id", unique=True, dropDups=True)
 
 def store(tweet, collection):
     db[collection].insert_one(tweet)
