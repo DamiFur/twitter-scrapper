1be23a3bc67b61671bc90aff7394f1d019bfe81b
damifur <damian.a.furman@gmail.com>
2019-07-19 16:45:38 -0300

    unique setup colleciton

--- mongo.py
+++ mongo.py
@@ -5,9 +5,10 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["tweets"]
 
 def setup(collection):
-    db[collection].create_index(
-        [('full_text', pymongo.TEXT)],
-        default_language='spanish', unique=True)
+    try:
+        db[collection].create_index(
+            [('full_text', pymongo.TEXT)],
+            default_language='spanish', unique=True)
 
 def setup_users():
     db.users.create_index("id", unique=True, dropDups=True)
